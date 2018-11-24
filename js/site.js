9ac6abf60d51cd7dbb4c08f8a270451cb28dbdb6
iocast <iocast@me.com>
2018-11-24 06:11:27 +0000

    Travis build 332 pushed to master

--- js/site.js
+++
@@ -1,9 +0,0 @@
-(function() {
-
-  document.querySelectorAll("button[name^=social-]").forEach(function(ele, idx) {
-    ele.addEventListener('click', function(evt) {
-      window.open(ele.dataset.url, "_blank");
-    }, false);
-  });
-
-})()
